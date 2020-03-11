0b4395d32a27cd5a7eb37bd91a6b97d4fcede237
claharotte <corentin.laharotte@insa-lyon.fr>
2020-03-11 17:54:08 +0100

    unlock ifcc and run the new grammar

--- SqueletteProjet/compiler/visitor.h
+++ SqueletteProjet/compiler/visitor.h
@@ -21,7 +21,7 @@ public:
 
   virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override {
 
-     int retval = stoi(ctx->CONST()->getText());
+     int retval = stoi(ctx->expr()->getText());
      std::cout<<".globl	main\n"
            " main: \n"
            " 	movl	$"<<retval<<", %eax\n"
@@ -29,6 +29,39 @@ public:
 
      return 0;
   }
+
+	virtual antlrcpp::Any visitBloc(ifccParser::BlocContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitDefinition(ifccParser::DefinitionContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitPar(ifccParser::ParContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitMult(ifccParser::MultContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitConst(ifccParser::ConstContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitName(ifccParser::NameContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitMoins(ifccParser::MoinsContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitPlus(ifccParser::PlusContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
 
 
 };
