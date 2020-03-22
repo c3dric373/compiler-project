35bc02fbf179d49ce531f77fe866daf62c1d3416
Félix CASTILLON <fx.ctln@gmail.com>
2020-03-22 15:38:33 +0000

    Update visitor.h

--- SqueletteProjet/compiler/visitor.h
+++ SqueletteProjet/compiler/visitor.h
@@ -17,88 +17,85 @@ class  Visitor : public ifccVisitor {
 public:
 
   virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override {
-    std::cout << "Axiom  FinAxiom" << std::endl;
     return visitChildren(ctx);
   }
 
   virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override {
-    std::cout << "Prog" << std::endl;
     AST::Bloc* astBloc = visit(ctx->bloc());
     AST::Expr::Expr* astExpr = visit(ctx->expr());
-//    auto truc = new AST::Prog(astBloc, astExpr);
-    std::cout << "FinProg" << std::endl;
     return new AST::Prog(astBloc, astExpr);
   }
 
-  virtual antlrcpp::Any visitBloc(ifccParser::BlocContext *ctx) override {
+  virtual antlrcpp::Any visitBlocinstr(ifccParser::BlocinstrContext *ctx) override {
     AST::Bloc* astBloc = new AST::Bloc();
-    for(auto& it : ctx->def()){
-      astBloc->pushDef(visit(it));
+    for(auto& it : ctx->instr()){
+      astBloc->pushInstr(visit(it));
     }
     return astBloc;
   }
 
-  virtual antlrcpp::Any visitDefexpr(ifccParser::DefexprContext *ctx) override {
-    std::cout << "DefExpr" << std::endl;
+  virtual antlrcpp::Any visitInstrdecl(ifccParser::InstrdeclContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitInstrdef(ifccParser::InstrdefContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitInstraffct(ifccParser::InstraffctContext *ctx) override {
+    return visitChildren(ctx);
+  }
+
+  virtual antlrcpp::Any visitDeclint(ifccParser::DeclintContext *ctx) override {
+    auto names = std::vector<std::string>();
+    for(auto& it : ctx->NAME()){
+      names.push_back(it->getText());
+    }
+    return new AST::Instr::Decl(names);
+  }
+
+  virtual antlrcpp::Any visitAffexpr(ifccParser::AffexprContext *ctx) override {
     AST::Expr::Expr* astExpr = visit(ctx->expr());
-//    auto truc = new AST::Def(ctx->NAME()->getText(), astExpr);
-    std::cout << "FinDefExpr" << std::endl;
-    return new AST::Def(ctx->NAME()->getText(), astExpr);
+    return new AST::Instr::Affct(ctx->NAME()->getText(), astExpr);
+  }
+
+  virtual antlrcpp::Any visitDefexpr(ifccParser::DefexprContext *ctx) override {
+    AST::Expr::Expr* astExpr = visit(ctx->expr());
+    return new AST::Instr::Def(ctx->NAME()->getText(), astExpr);
   }
 
   virtual antlrcpp::Any visitAdd(ifccParser::AddContext *ctx) override {
-    std::cout << "Add" << std::endl;
     AST::Expr::Expr* astLeftExpr = visit(ctx->expr()[0]);
     AST::Expr::Expr* astRightExpr = visit(ctx->expr()[1]);
-//    auto truc = new AST::Expr::Add(astLeftExpr, astRightExpr);
-    std::cout << "FinAdd" << std::endl;
     return (AST::Expr::Expr*)new AST::Expr::Add(astLeftExpr, astRightExpr);
   }
 
   virtual antlrcpp::Any visitSub(ifccParser::SubContext *ctx) override {
-    std::cout << "Sub" << std::endl;
     AST::Expr::Expr* astLeftExpr = visit(ctx->expr()[0]);
     AST::Expr::Expr* astRightExpr = visit(ctx->expr()[1]);
-    std::cout << "Ozymandias" << std::endl;
-//    auto truc = new AST::Expr::Sub(astLeftExpr, astRightExpr);
-    std::cout << "FinSub" << std::endl;
     return (AST::Expr::Expr*)new AST::Expr::Sub(astLeftExpr, astRightExpr);
   }
 
   virtual antlrcpp::Any visitMinus(ifccParser::MinusContext *ctx) override {
-    std::cout << "Minus" << std::endl;
     AST::Expr::Expr* astExpr = visit(ctx->expr());
-//    auto truc = new AST::Expr::Minus(astExpr);
-    std::cout << "FinMinus" << std::endl;
     return (AST::Expr::Expr*)new AST::Expr::Minus(astExpr);
   }
 
   virtual antlrcpp::Any visitMult(ifccParser::MultContext *ctx) override {
-    std::cout << "Mult" << std::endl;
     AST::Expr::Expr* astLeftExpr = visit(ctx->expr()[0]);
     AST::Expr::Expr* astRightExpr = visit(ctx->expr()[1]);
-//    auto truc = new AST::Expr::Mult(astLeftExpr, astRightExpr);
-    std::cout << "FinMult" << std::endl;
     return (AST::Expr::Expr*)new AST::Expr::Mult(astLeftExpr, astRightExpr);
   }
 
   virtual antlrcpp::Any visitPar(ifccParser::ParContext *ctx) override {
-    std::cout << "Par  FinPar" << std::endl;
     return visit(ctx->expr());
   }
 
   virtual antlrcpp::Any visitConst(ifccParser::ConstContext *ctx) override {
-    std::cout << "Const" << std::endl;
-    std::cout << ctx->CONST()->getText() << '|' << std::endl;
-//    auto truc = new AST::Expr::Const(std::stoi(ctx->CONST()->getText()));
-    std::cout << "FinConst" << std::endl;
     return (AST::Expr::Expr*)new AST::Expr::Const(std::stoi(ctx->CONST()->getText()));
   }
 
   virtual antlrcpp::Any visitName(ifccParser::NameContext *ctx) override {
-    std::cout << "Name" << std::endl;
-//    auto truc = new AST::Expr::Name(ctx->NAME()->getText());
-    std::cout << "FinName" << std::endl;
     return (AST::Expr::Expr*)new AST::Expr::Name(ctx->NAME()->getText());
   }
 };
