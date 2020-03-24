0beebf9fa24d956d0a8d7cb92e4bd1afe8d889f3
cmc373 <milinaire.c@gmail.com>
2020-03-24 15:17:10 +0100

    test wip

--- SqueletteProjet/tests/tests/Init/4_defdeclexpr.c
+++ SqueletteProjet/tests/tests/Init/4_defdeclexpr.c
@@ -1,8 +1,4 @@
-
 int main(){
-    int a = 5+7+8-9*12;
-    int b = 12*8-6;
-    int c = b*c;
-    int d = a+c;
-    return 12*d-5;
+    int a = 5-2;
+    return a;
 }
--- SqueletteProjet/tests/tests/Init/5_definition.c
+++ SqueletteProjet/tests/tests/Init/5_definition.c
@@ -1,5 +1,3 @@
-
-
 int main(){
     int a = 5;
     return a;
