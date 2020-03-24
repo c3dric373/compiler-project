aa52235be45304ac3a566fc62aa454e075537390
cmc373 <milinaire.c@gmail.com>
2020-03-24 16:08:54 +0100

    addsubmultTest

---
+++ SqueletteProjet/tests/tests/Init/addmultsub6.c
@@ -0,0 +1,6 @@
+int main(){
+    int a = 1;
+    int b = 2+a;
+    int c= 3-a*b;
+    return c;
+}
---
+++ SqueletteProjet/tests/tests/Init/addmultsub9.c
@@ -0,0 +1,7 @@
+int main(){
+    int a = 1;
+    int b = 2+a;
+    int c= 3-a*b;
+    int d = a+b-c+d;
+    return d;
+}
