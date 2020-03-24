6d849eb1eed5d2b43d27eaa0a1044f020ee29f40
cmc373 <milinaire.c@gmail.com>
2020-03-24 16:18:58 +0100

    test auto affectation

--- SqueletteProjet/compiler/ret42.c
+++ SqueletteProjet/compiler/ret42.c
@@ -1,7 +1,8 @@
 int main(){
-    int a = 5;
-    return a;
+    int a = c;
+
 }
+
 
 
 
--- SqueletteProjet/tests/tests/Init/addmultsub7.c
+++
@@ -1,7 +0,0 @@
-int main(){
-    int a = 1;
-    int b = 2+a;
-    int c= 3-a*b;
-    int d = a+b-c+d;
-    return d;
-}
