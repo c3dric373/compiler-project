7d292cca418bbfa1d9c0ce3cc5babf9c2972c223
cmc373 <milinaire.c@gmail.com>
2020-03-27 15:16:50 +0100

    commit to see if pipeline works remotyle

--- SqueletteProjet/tests/test_if.sh
+++ SqueletteProjet/tests/test_if.sh
@@ -1,3 +1,4 @@
 chmod 755 pld-wrapper.sh
-python3 pld-test.py tests/Init/
+python3 pld-test.py tests/Init/ | tee test-result.txt
 rm -rf pld-test-output
+exit $(! cat test-result.txt | grep "TEST FAIL")
