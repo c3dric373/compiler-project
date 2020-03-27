2f8cf968d8fc648a0cb672317b727c248957de58
cmc373 <milinaire.c@gmail.com>
2020-03-27 20:00:05 +0100

    changed ret42.c to a good example

--- SqueletteProjet/compiler/ret42.c
+++ SqueletteProjet/compiler/ret42.c
@@ -1,7 +1,9 @@
 int main(){
     int a = 3;
-    int b = 5+6;
-    int c = 5*b;
-    return c;
+    int b = (5+1+1*1)+(5-5*0)-6;
+    int c,d;
+    c = 5*b;
+    d = 4-1*(5*(1-1)+1);
+    return a+b*c-d;
 }
 
