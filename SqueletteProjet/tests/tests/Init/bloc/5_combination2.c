d000c71ef1dfaaf060202065d6071745343d5c5f
Grazia <graziagiuliaribbeni@gmail.com>
2020-04-05 13:30:22 +0200

    grazia

---
+++ SqueletteProjet/tests/tests/Init/bloc/5_combination2.c
@@ -0,0 +1,17 @@
+int main(){
+    int a = 6 ;
+    int b = 8 | a;
+    int d =2*a+b;
+    int e,f;
+    while (a <= 16){
+        if(b==0){
+            a = a+1;
+        }else
+	    b=8 | a;
+	    e=a;
+        }
+        
+    }
+    f=(a+b)*e-d+1;
+    return f;
+}
---
+++ SqueletteProjet/tests/tests/Init/char/3_binary
@@ -0,0 +1,11 @@
+int main(){
+    
+    char a='a';
+    char b,c,d;
+    b='b';
+    c=a&b;
+    d=a|b;
+    char e=c^d;
+    char f=!c;
+    return f;
+}
