946e80e032ce96bb9aa04b4e713b8bf5c4b7b0af
Diskane <diskaneht@hotmail.fr>
2020-04-06 13:25:38 +0200

    return tab char + opération(add,sub) ne fonctionne pas

--- SqueletteProjet/compiler/ret42.s
+++ SqueletteProjet/compiler/ret42.s
@@ -2,17 +2,30 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	movl $0, -20(%rbp)
-	movl -20(%rbp), %eax
-	movl %eax , -4(%rbp) # 0a
-	movl -4(%rbp), %eax
-	movl %eax , -8(%rbp) # 1a
-	movl $3, -24(%rbp)
-	movl -24(%rbp), %eax
-	movl %eax , -12(%rbp) # 2a
-	movl $4, -28(%rbp)
-	movl -28(%rbp), %eax
-	movl %eax , -16(%rbp) # 3a
-	movl -12(%rbp), %eax
+	movl $1, -7(%rbp)
+	movl -7(%rbp), %eax
+	movl %eax , -1(%rbp) # 0a
+	movl $2, -11(%rbp)
+	movl -11(%rbp) , %eax
+	addl -1(%rbp), %eax # !tmp11 + 0a
+	movl %eax, -15(%rbp)
+	movl -15(%rbp), %eax
+	movl %eax , -2(%rbp) # 1a
+	movl $2, -19(%rbp)
+	movl -19(%rbp) , %eax
+	imull -1(%rbp), %eax # !tmp19 * 0a
+	movl %eax, -23(%rbp)
+	movl $12, -27(%rbp)
+	movl -27(%rbp) , %eax
+	imull -2(%rbp), %eax # !tmp27 * 1a
+	movl %eax, -31(%rbp)
+	movl -23(%rbp) , %eax
+	addl -31(%rbp), %eax # !tmp23 + !tmp31
+	movl %eax, -35(%rbp)
+	movl -35(%rbp), %eax
+	movl %eax , -3(%rbp) # 2a
+	movl -1(%rbp) , %eax
+	addl -2(%rbp), %eax # 0a + 1a
+	movl %eax, -39(%rbp)
 	popq %rbp
 	ret
