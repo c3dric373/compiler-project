27421bfcac16db116b8cfdf443f309d850cb33eb
cmc373 <milinaire.c@gmail.com>
2020-04-06 10:05:14 +0200

    fix

--- SqueletteProjet/compiler/.s
+++ SqueletteProjet/compiler/.s
@@ -2,12 +2,9 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	movl $5, -1(%rbp)
+	movl $2, -4(%rbp)
+	movl -4(%rbp), %eax
+	movl %eax , -1(%rbp) # a
 	movl -1(%rbp), %eax
-	NEG %eax
-	movl %eax, -1(%rbp)
-	movl -1(%rbp), %eax
-	movl %eax , -12(%rbp)
-	movl -12(%rbp), %eax
 	popq %rbp
 	ret
