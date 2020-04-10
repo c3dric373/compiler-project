d619cc8c39a68b690ea8a07896ae1d584753f1f3
cmc373 <milinaire.c@gmail.com>
2020-04-10 16:08:50 +0200

    if fixed

--- SqueletteProjet/compiler/ret42.s
+++ SqueletteProjet/compiler/ret42.s
@@ -2,7 +2,7 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $120, %rsp
+	subq $152, %rsp
 	movl $20, -4(%rbp)
 	movl -4(%rbp), %eax
 	movl %eax , -8(%rbp) # a
@@ -62,36 +62,66 @@ main:
 	movl $125, -88(%rbp)
 	movl -88(%rbp), %eax
 	movl %eax , -92(%rbp) # m
-	movl -8(%rbp) , %eax
-	addl -16(%rbp), %eax # a + b
+	movl -16(%rbp), %eax
+	cmp  %eax, -8(%rbp)
+	setb %dl # a<b
+	movzbl %dl, %eax
 	movl %eax, -96(%rbp)
-	cmpl $1, -96(%rbp)
-	je .Lmain0
-	jmp .Lmain1
+	movl -36(%rbp), %eax
+	cmp  %eax, -48(%rbp)
+	seta %dl # e>d
+	movzbl %dl, %eax
+	movl %eax, -100(%rbp)
+	movl -48(%rbp), %eax
+	cmp  %eax, -64(%rbp)
+	seta %dl # f>e
+	movzbl %dl, %eax
+	movl %eax, -104(%rbp)
+	movl -100(%rbp) , %eax
+	imull -104(%rbp), %eax # !tmp100 * !tmp104
+	movl %eax, -108(%rbp)
+	movl -96(%rbp) , %eax
+	and -108(%rbp), %eax # !tmp96 and !tmp108
+	movl %eax, -112(%rbp)
+	movl -36(%rbp), %eax
+	cmp  %eax, -24(%rbp)
+	seta %dl # c>d
+	movzbl %dl, %eax
+	movl %eax, -116(%rbp)
+	movl $257, -120(%rbp)
+	movl -116(%rbp) , %eax
+	addl -120(%rbp), %eax # !tmp116 + !tmp120
+	movl %eax, -124(%rbp)
+	movl -112(%rbp) , %eax
+	or -124(%rbp), %eax # !tmp112 or!tmp124
+	movl %eax, -128(%rbp)
+	cmpl $0, -128(%rbp)
+	je .Lmain1
+	jmp .Lmain0
 .Lmain0: 
 	movl -16(%rbp), %eax
 	cmpl  %eax, -24(%rbp)
 	sete %dl # b==c
 	movzbl %dl, %eax
-	movl %eax, -100(%rbp)
+	movl %eax, -132(%rbp)
 	movl -48(%rbp) , %eax
 	and -8(%rbp), %eax # e and a
-	movl %eax, -104(%rbp)
-	movl -100(%rbp) , %eax
-	imull -104(%rbp), %eax # !tmp100 * !tmp104
-	movl %eax, -108(%rbp)
-	movl -108(%rbp) , %eax
-	addl -8(%rbp), %eax # !tmp108 + a
-	movl %eax, -112(%rbp)
-	movl -112(%rbp), %eax
+	movl %eax, -136(%rbp)
+	movl -132(%rbp) , %eax
+	imull -136(%rbp), %eax # !tmp132 * !tmp136
+	movl %eax, -140(%rbp)
+	movl -140(%rbp) , %eax
+	addl -8(%rbp), %eax # !tmp140 + a
+	movl %eax, -144(%rbp)
+	movl -144(%rbp), %eax
 	movl %eax , -92(%rbp) # m
-	movl $78, -116(%rbp)
-	movl -116(%rbp), %eax
-	movl %eax , -120(%rbp) # w
+	movl $78, -148(%rbp)
+	movl -148(%rbp), %eax
+	movl %eax , -152(%rbp) # w
 .Lmain1: 
 	movl -92(%rbp), %eax
 	 jmp .main_ret
 .main_ret:
-	addq $120, %rsp
+	addq $152, %rsp
 	popq %rbp
 	ret
