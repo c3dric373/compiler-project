22421727a82f43038a564877db36b0370bd67099
Félix CASTILLON <fx.ctln@gmail.com>
2020-04-10 04:41:07 +0000

    Update ret42.c

--- SqueletteProjet/compiler/ret42.c
+++ SqueletteProjet/compiler/ret42.c
@@ -1,15 +1,5 @@
-int factorielle(int valeur) {
-    if (valeur == 0) {
-        return 1;
-    } else {
-        int b = valeur -1;
-        return (valeur * factorielle(b));
-    }
-}
-
-int main() {
-    int a = 7;
-    int b;
-    b = factorielle(a);
-    return b;
+int main(){
+    char a = 'd';
+    char c='e';
+    return a-c;
 }
