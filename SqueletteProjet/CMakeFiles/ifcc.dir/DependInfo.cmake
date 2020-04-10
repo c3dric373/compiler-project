46172eaac2cfae66f7e46e6f777eb57543a96cf6
Diskane <diskaneht@hotmail.fr>
2020-04-10 18:33:18 +0200

    test

---
+++ SqueletteProjet/CMakeFiles/ifcc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/AST/AST.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/AST/AST.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/Erreur/Erreur.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/Erreur/Erreur.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/IR/IR.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/IR/IR.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/antlr4-generated/ifccBaseVisitor.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccBaseVisitor.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/antlr4-generated/ifccLexer.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccLexer.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/antlr4-generated/ifccParser.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccParser.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/antlr4-generated/ifccVisitor.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccVisitor.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/main.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/main.cpp.o"
+  "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/compiler/visitor.cpp" "/home/diskane/CLionProjects/compiler-project/SqueletteProjet/CMakeFiles/ifcc.dir/compiler/visitor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/antlr4-runtime"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SqueletteProjet/CMakeFiles/ifcc.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ifcc.dir/compiler/AST/AST.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/Erreur/Erreur.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/IR/IR.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccBaseVisitor.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccLexer.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccParser.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/antlr4-generated/ifccVisitor.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/main.cpp.o"
+  "CMakeFiles/ifcc.dir/compiler/visitor.cpp.o"
+  "compiler/ifcc"
+  "compiler/ifcc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ifcc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
